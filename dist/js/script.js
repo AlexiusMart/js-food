ec71b1dc14a2d8c35e21bdc7da46835e9b2e6934
iStre1ok <am-nf@yandex.ru>
2022-11-16 16:15:07 +0300

    toggle tabs slide

--- dist/js/script.js
+++ dist/js/script.js
@@ -91,9 +91,43 @@
   !*** ./src/js/main.js ***!
   \************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
 
 
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContents = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
+  function hideTabContent() {
+    tabsContents.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+  function showTabContent(i) {
+    tabsContents[i].classList.add('show', 'fade');
+    tabsContents[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  hideTabContent();
+  showTabContent(0);
+  tabsParent.addEventListener('click', event => {
+    const target = event.target;
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == tabs[i]) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
 
 /***/ })
 
