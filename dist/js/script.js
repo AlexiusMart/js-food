37860ece376cb1b5e09e6fea077fdfdcf3203312
iStre1ok <am-nf@yandex.ru>
2022-11-23 16:43:57 +0300

    compelte timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -97,6 +97,7 @@
 
 
 window.addEventListener('DOMContentLoaded', () => {
+  // tabs
   const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContents = document.querySelectorAll('.tabcontent'),
     tabsParent = document.querySelector('.tabheader__items');
@@ -127,6 +128,57 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  // timer
+  const endDate = '2022-11-27';
+  function getTimeRemaining(lastDate) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(lastDate) - Date.parse(new Date());
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor(t / 1000 / 60 / 60 % 24);
+      minutes = Math.floor(t / 1000 / 60 % 60);
+      seconds = Math.floor(t / 1000 % 60);
+    }
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    }
+    return num;
+  }
+  function setTimer(selector, endDate) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      dataInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endDate);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+      if (t.total <= 0) {
+        clearInterval(dataInterval);
+      }
+    }
+  }
+  setTimer('.timer', endDate);
 });
 
 /***/ })
